a0d830d1e416490bb170242f46eb0a8e8858ebe2
HIEPTN\HiepTN <hieptn210620@gmail.com>
2021-10-11 14:06:56 +0700

    icon

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import Profile from './src/modules/Profile/Profile';
 import { NativeBaseProvider } from 'native-base';
 import UploadIcon from 'react-native-vector-icons/MaterialIcons';
 import CameraIcon from 'react-native-vector-icons/Ionicons';
+import Test from './src/modules/Test/Test';
 
 const Tab = createBottomTabNavigator();
 
@@ -26,6 +27,14 @@ const App = () => {
                     <Tab.Screen
                         name='Upload File'
                         component={Profile}
+                        options={{
+                            tabBarLabel: 'Upload File',
+                            tabBarIcon: ({ size }) => <UploadIcon name="upload-file" size={28} />
+                        }}
+                    />
+                    <Tab.Screen
+                        name='Test'
+                        component={Test}
                         options={{
                             tabBarLabel: 'Upload File',
                             tabBarIcon: ({ size }) => <UploadIcon name="upload-file" size={28} />
